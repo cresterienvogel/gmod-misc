3510977028e83677a5653d9f803c24dea899bb9e
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-08-07 01:19:05 +0700

    Add files via upload

---
+++ countries.lua
@@ -0,0 +1,97 @@
+--[[
+    Global variables for instant and comfortable access to presented in Garry's mod country icons.
+    ## Country codes are made according to Country Codes Alpha-2.
+]]
+
+BULGARIA = Material("resource/localization/bg.png")
+BG = BULGARIA
+
+CZECH = Material("resource/localization/cs.png")
+CZ = CZECH
+
+DENMARK = Material("resource/localization/da.png")
+DK = DENMARK
+
+GERMANY = Material("resource/localization/de.png")
+DE = GERMANY
+
+GREECE = Material("resource/localization/el.png")
+GR = GREECE
+
+BRITAIN = Material("resource/localization/en.png")
+GB = BRITAIN
+
+SPAIN = Material("resource/localization/es-ES.png")
+ES = SPAIN
+
+ESTONIA = Material("resource/localization/et.png")
+EE = ESTONIA
+
+FINLAND = Material("resource/localization/fi.png")
+FI = FINLAND
+
+FRANCE = Material("resource/localization/fr.png")
+FR = FRANCE
+
+ISRAEL = Material("resource/localization/he.png")
+IL = ISRAEL
+
+CROATIA = Material("resource/localization/hr.png")
+HR = CROATIA
+
+HUNGARY = Material("resource/localization/hu.png")
+HU = HUNGARY
+
+ITALY = Material("resource/localization/it.png")
+IT = ITALY
+
+JAPAN = Material("resource/localization/ja.png")
+JP = JAPAN
+
+KOREA = Material("resource/localization/ko.png")
+KR = KOREA
+
+LITHUANIA = Material("resource/localization/lt.png")
+LT = LITHUANIA
+
+NETHERLANDS = Material("resource/localization/nl.png")
+NL = NETHERLANDS
+
+NORWAY = Material("resource/localization/no.png")
+NO = NORWAY
+
+POLAND = Material("resource/localization/pl.png")
+PL = POLAND
+
+BRAZIL = Material("resource/localization/pt-BR.png")
+BR = BRAZIL
+
+PORTUGAL = Material("resource/localization/pt-PT.png")
+PT = PORTUGAL
+
+SLOVAKIA = Material("resource/localization/sk.png")
+SK = SLOVAKIA
+
+RUSSIA = Material("resource/localization/ru.png")
+RU = RUSSIA
+
+SWEDEN = Material("resource/localization/sv-SE.png")
+SE = SWEDEN
+
+THAILAND = Material("resource/localization/th.png")
+TH = THAILAND
+
+TURKEY = Material("resource/localization/tr.png")
+TR = TURKEY
+
+UKRAINE = Material("resource/localization/uk.png")
+UA = UKRAINE
+
+VIETNAM = Material("resource/localization/vi.png")
+VN = VIETNAM
+
+TAIWAN = Material("resource/localization/zh-TW.png")
+TW = TAIWAN
+
+CHINA = Material("resource/localization/zh-CN.png")
+CN = CHINA
