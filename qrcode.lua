83862977536e3449149fe080e83f68e1c469ba8b
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-11-17 13:15:07 +0700

    Add files via upload

---
+++ qrcode.lua
@@ -0,0 +1,25 @@
+--[[
+	Creating a QR Code takes some time,
+		use some timer or something.
+]]
+
+function QRCode(data, size)
+	local id = util.Base64Encode(data)
+
+	size = size and size or 256
+	data = string.Explode(" ", data)
+	data = table.concat(data, "%20")
+
+	if not file.Exists("qr", "DATA") then
+		file.CreateDir("qr")
+	end
+
+	if file.Exists("qr/" .. id .. ".png", "DATA") then
+		return "data/qr/" .. id .. ".png"
+	else
+		http.Fetch("https://api.qrserver.com/v1/create-qr-code/?size=" .. size .. "x" .. size .. "&data=" .. data, function(content)
+			file.Write("qr/" .. id .. ".png", content)
+		end)
+		return "data/qr/" .. id .. ".png"
+	end
+end
