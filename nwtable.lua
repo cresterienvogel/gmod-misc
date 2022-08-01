a85db2451bf301d817a23f5838b64bdca503ecb8
crester <35332822+cresterienvogel@users.noreply.github.com>
2022-08-02 06:12:51 +0700

    Update nwtable.lua

--- nwtable.lua
+++ nwtable.lua
@@ -1,6 +1,22 @@
 if SERVER then
 	util.AddNetworkString("NWTable Broadcast")
-	util.AddNetworkString("NWTable Broadcast Table")
+	util.AddNetworkString("NWTable Broadcast Tables")
+
+	hook.Add("PlayerInitialSpawn", "NWTable Broadcast", function(pl)
+		timer.Simple(1, function()
+			if not IsValid(pl) then
+				return
+			end
+			for _, ent in ipairs(ents.GetAll()) do
+				if ent.NWTables then
+					net.Start("NWTable Broadcast Tables")
+						net.WriteEntity(ent)
+						net.WriteTable(ent.NWTables)
+					net.Send(pl)
+				end
+			end
+		end)
+	end)
 else
 	net.Receive("NWTable Broadcast", function()
 		local ent = net.ReadEntity()
@@ -11,7 +27,7 @@ else
 		ent.NWTables[name] = tbl
 	end)
 
-	net.Receive("NWTable Broadcast Table", function()
+	net.Receive("NWTable Broadcast Tables", function()
 		local ent = net.ReadEntity()
 		local tbl = net.ReadTable()
 
@@ -20,18 +36,17 @@ else
 end
 
 local meta = FindMetaTable("Entity")
-function meta:SetNWTable(name, value, key)
+
+function meta:AddNWTableVal(name, value, key)
 	self.NWTables = self.NWTables or {}
 	if not self.NWTables[name] then
 		self.NWTables[name] = {}
 	end
 
-	if key ~= nil or value ~= nil then
-		if key then
-			self.NWTables[name][key] = value
-		else
-			table.insert(self.NWTables[name], value)
-		end
+	if key then
+		self.NWTables[name][key] = value
+	else
+		table.insert(self.NWTables[name], value)
 	end
 
 	if SERVER then
@@ -41,19 +56,45 @@ function meta:SetNWTable(name, value, key)
 			net.WriteTable(self.NWTables[name])
 		net.Broadcast()
 	end
+
+	return self.NWTables[name]
+end
+
+function meta:RemoveNWTableVal(name, value)
+	self.NWTables = self.NWTables or {}
+	if not self.NWTables[name] then
+		self.NWTables[name] = {}
+	end
+
+	local key = table.KeyFromValue(self.NWTables[name], value)
+	self.NWTables[name][key] = nil
+
+	if SERVER then
+		net.Start("NWTable Broadcast")
+			net.WriteEntity(self)
+			net.WriteString(name)
+			net.WriteTable(self.NWTables[name])
+		net.Broadcast()
+	end
+
+	return self.NWTables[name]
+end
+
+function meta:SetNWTable(name, tbl)
+	self.NWTables = self.NWTables or {}
+	self.NWTables[name] = tbl
+
+	if SERVER then
+		net.Start("NWTable Broadcast")
+			net.WriteEntity(self)
+			net.WriteString(name)
+			net.WriteTable(self.NWTables[name])
+		net.Broadcast()
+	end
+
+	return self.NWTables[name]
 end
 
 function meta:GetNWTable(name)
 	return (self.NWTables and self.NWTables[name]) or {}
 end
-
-hook.Add("PlayerInitialSpawn", "NWTable Broadcast", function(pl)
-	for _, ent in ipairs(ents.GetAll()) do
-		if ent.NWTables then
-			net.Start("NWTable Broadcast Table")
-				net.WriteEntity(ent)
-				net.WriteTable(ent.NWTables)
-			net.Send(pl)
-		end
-	end
-end)
